3fdc3a4380023b5ef27be59e6f3c1e79b206f9bf
Olopper <olopper2407@g.educaand.es>
2025-01-24 13:54:06 +0100

    Exercise 4

---
+++ Ejercicio 4.py
@@ -0,0 +1,6 @@
+n1=int(input("Introduce el primer número "))
+n2=int(input("Introduce el segundo número "))
+n3=int(input("Introduce el tercer número "))
+suma = n1+n2+n3
+promedio = n1+n2+n3/3
+print("La suma de los número es", suma ,"El promedio es",promedio)
