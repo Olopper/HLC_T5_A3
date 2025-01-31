648ea810d5d63ccb448c213b79811d437e8acf2d
Olopper <olopper2407@g.educaand.es>
2025-01-31 13:41:19 +0100

    Exercise 10

---
+++ Ejercicio 10.py
@@ -0,0 +1,8 @@
+def pares():
+    lista = [1, 2, 3, 4, 5, 6]
+    for lita in lista:
+        if lita:
+            lista = [1, 3, 5]
+            lista.remove
+            print(lista)
+pares()
---
+++ Ejercicio 7.py
@@ -0,0 +1,5 @@
+def divisores():
+    lista = []
+    for i in range(numero, numero+1):
+
+numero=int(input("Introduce un número: "))
